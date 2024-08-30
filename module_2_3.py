7f39487bf7b184b0887df6acb8e1270e72e6366c
Buzov9 <koskinfedor9@gmail.com>
2024-08-30 19:37:54 +0800

    hg

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+non = 0
+while non < len(my_list) and my_list[non] >= -1:
+    if my_list[non] != 0:
+        print(my_list[non])
+    non += 1
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
