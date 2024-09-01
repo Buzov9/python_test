806bbe4640964d1dd9ed6397fcc3da333345e26f
Buzov9 <koskinfedor9@gmail.com>
2024-09-01 23:16:31 +0800

    модуль 2.4

--- module_2_4.py
+++ module_2_4.py
@@ -1,7 +1,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in numbers:
+for i in numbers[1::]: # надеюсь это честно
     is_prime = True
     for j in range(2, int(i**0.5)+1):
         if i % j == 0:
