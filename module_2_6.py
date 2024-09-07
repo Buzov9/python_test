52b54b2185f203b72089bcb3f4ec55fac9cb79c6
Buzov9 <koskinfedor9@gmail.com>
2024-09-07 22:51:23 +0800

    модуль 2.6

---
+++ module_2_6.py
@@ -0,0 +1,18 @@
+#n = int(input("число на первой вставке: "))
+n = 6
+
+result = []
+if n % 2 == 0:
+    n2 = n/2
+    n2 = int(n2)
+    for i1 in range(1, n2 + 1):
+        for j1 in range(i1 + 1, n2 + 1):
+            if (i1 + j1) == n2:
+                result.append((i1, j1))
+
+for i in range(1, n+1):
+    for j in range(i+1, n+1):
+        if (i + j) == n:
+            result.append((i, j))
+print(*result)
+print(f'{result}')
