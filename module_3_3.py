428082c8aa161e399f6a38d3478aaf9e57d86dd5
Buzov9 <koskinfedor9@gmail.com>
2024-09-09 19:56:58 +0800

    модуль 3.3

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='str', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [1.5, 2, 'word']
+values_dict = {'a':False, 'b':11, 'c':'1c'}
+print_params(values_list)
+print_params(values_dict)
+
+values_list_2 = [4.5, 'False']
+print_params(*values_list_2, 42)
