f0a5be4d9c80192a9a4de1531e292fbae0f120cb
Buzov9 <koskinfedor9@gmail.com>
2024-09-10 22:32:12 +0800

    модуль 3.4

--- study/module_3_4.py
+++ study/module_3_4.py
@@ -2,6 +2,7 @@ def single_root_word(root_word, *other_word):
     same_words = []
     root_word = root_word.lower()
     other_word_low = [i.lower() for i in other_word]
+
     for i in other_word_low:
         if i in root_word or root_word in i:
             same_words.append(i)
