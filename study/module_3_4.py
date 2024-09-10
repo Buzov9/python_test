0372e06105e1669d3a383910ebc4a955e2c67614
Buzov9 <koskinfedor9@gmail.com>
2024-09-10 22:30:13 +0800

    модуль 3.4

---
+++ study/module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_word(root_word, *other_word):
+    same_words = []
+    root_word = root_word.lower()
+    other_word_low = [i.lower() for i in other_word]
+    for i in other_word_low:
+        if i in root_word or root_word in i:
+            same_words.append(i)
+    return same_words
+
+
+result1 = single_root_word('rich', 'richest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_word('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
