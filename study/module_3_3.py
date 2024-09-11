1775f02bd4abd40e3b03bd5ed72eec37c49774e5
Buzov9 <koskinfedor9@gmail.com>
2024-09-11 22:04:05 +0800

    11 *

--- study/module_3_3.py
+++ study/module_3_3.py
@@ -7,9 +7,9 @@ print_params(b=25)
 print_params(c=[1, 2, 3])
 
 values_list = [1.5, 2, 'word']
-values_dict = {'a':False, 'b':11, 'c':'1c'}
-print_params(values_list)
-print_params(values_dict)
+values_dict = {'a': False, 'b': 11, 'c': '1c'}
+print_params(*values_list)
+print_params(**values_dict)
 
 values_list_2 = [4.5, 'False']
 print_params(*values_list_2, 42)
